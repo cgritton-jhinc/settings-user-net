70c002a2680f8cec2361362ff258417191915dda
Christopher B. Gritton <cgritton@jhoe.com>
2017-07-31 15:31:24 -0700

    Starting a project to use github repository.

---
+++ cgritton.libs.net.settings/cgritton.libs.net.settings/IConfigurations.cs
@@ -0,0 +1,75 @@
+﻿// ***********************************************************************
+// Assembly         : cgritton.libs.net.settings
+// Author           : cgritton-jhinc
+// Created          : 07-31-2017
+//
+// Last Modified By : cgritton-jhinc
+// Last Modified On : 07-31-2017
+// ***********************************************************************
+// <copyright file="IConfigurations.cs" company="">
+//     Copyright ©  2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System.Collections.Generic;
+
+namespace cgritton.libs.net.settings
+{
+    /// <summary>
+    /// Interface IConfigurations
+    /// </summary>
+    public interface IConfigurations
+    {
+
+        /// <summary>
+        /// Writes the value.
+        /// </summary>
+        /// <param name="output">The output.</param>
+        /// <param name="field">The field.</param>
+        /// <param name="value">The value.</param>
+        void WriteValue(System.IO.FileInfo output, string field, string value);
+        /// <summary>
+        /// Writes the value.
+        /// </summary>
+        /// <param name="output">The output.</param>
+        /// <param name="section">The section.</param>
+        /// <param name="field">The field.</param>
+        /// <param name="value">The value.</param>
+        void WriteValue(System.IO.FileInfo output, string section, string field, string value);
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="field">The field.</param>
+        /// <returns>System.String.</returns>
+        string GetValue(System.IO.FileInfo input, string field);
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="section">The section.</param>
+        /// <param name="field">The field.</param>
+        /// <returns>System.String.</returns>
+        string GetValue(System.IO.FileInfo input, string section, string field);
+        /// <summary>
+        /// Deletes the section.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="section">The section.</param>
+        void DeleteSection(System.IO.FileInfo input, string section);
+        /// <summary>
+        /// Gets the section keys.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <param name="section">The section.</param>
+        /// <returns>List&lt;System.String&gt;.</returns>
+        List<string> GetSectionKeys(System.IO.FileInfo input, string section);
+        /// <summary>
+        /// Gets the sections.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>List&lt;System.String&gt;.</returns>
+        List<string> GetSections(System.IO.FileInfo input);
+
+    }
+}
---
+++ cgritton.libs.net.settings/cgritton.libs.net.settings/xml/configuration/XmlConfigAttribute.cs
@@ -0,0 +1,68 @@
+﻿// ***********************************************************************
+// Assembly         : cgritton.libs.net.settings
+// Author           : cgritton-jhinc
+// Created          : 07-31-2017
+//
+// Last Modified By : cgritton-jhinc
+// Last Modified On : 07-31-2017
+// ***********************************************************************
+// <copyright file="XmlConfigAttribute.cs" company="">
+//     Copyright ©  2017
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+
+/// <summary>
+/// The configuration namespace.
+/// </summary>
+namespace cgritton.libs.net.settings.xml.configuration
+{
+    /// <summary>
+    /// Class XmlConfigAttribute.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [System.AttributeUsage(AttributeTargets.Class)]
+    public class XmlConfigAttribute : System.Attribute
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="xmlconfigattribute" /> class.
+        /// </summary>
+        /// <param name="rootelem">The root element.</param>
+        /// <param name="settingselem">The settings element.</param>
+        public XmlConfigAttribute(string rootelem, string settingselem)
+        {
+            root = rootelem;
+            settings = settingselem;
+        }
+
+        /// <summary>
+        /// The root
+        /// </summary>
+        private string root = "cgritton.libs.net.settings";
+        /// <summary>
+        /// The settings
+        /// </summary>
+        private string settings = "global";
+
+        /// <summary>
+        /// Gets the root element.
+        /// </summary>
+        /// <value>The root element.</value>
+        public string RootElement
+        {
+            get { return root; }
+        }
+
+        /// <summary>
+        /// Gets the settings element.
+        /// </summary>
+        /// <value>The settings element.</value>
+        public string SettingsElement
+        {
+            get { return settings; }
+        }
+
+    }
+}

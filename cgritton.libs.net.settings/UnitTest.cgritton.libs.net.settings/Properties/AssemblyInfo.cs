569adc30f143b75fe53afc97ac24e31b17df983c
Christopher B. Gritton <19963558+cgritton-jhinc@users.noreply.github.com>
2017-07-31 16:33:28 -0700

    Revert "Revert "Starting a project to use github repository.""
    This reverts commit 20cf24bb863d101c2ba5a50970bc84f11fa663a5.

---
+++ cgritton.libs.net.settings/UnitTest.cgritton.libs.net.settings/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest.cgritton.libs.net.settings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest.cgritton.libs.net.settings")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7335c164-88e3-4c17-88f0-2547920796f8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ cgritton.libs.net.settings/UnitTest.cgritton.libs.net.settings/UnitTestXmlConfigWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cgritton.libs.net.settings.xml.configuration;
+using cgritton.libs.net.settings;
+
+namespace UnitTest.cgritton.libs.net.settings
+{
+    [TestClass]
+    public class UnitTestXmlConfigWriter
+    {
+        string testDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\cgritton.libs.net.settings.unittest";
+
+        [TestInitialize()]
+        public void InitTestDirectory()
+        {           
+            if (!System.IO.Directory.Exists(testDir)) System.IO.Directory.CreateDirectory(testDir);
+        }
+
+        [TestCleanup()]
+        public void DestroyTestDirectory()
+        {
+           if (System.IO.Directory.Exists(testDir)) System.IO.Directory.Delete(testDir, true);
+        }
+
+        [TestMethod]
+        public void WriteValueNoSection()
+        {
+            //set variables
+            string field = "my-app-field";
+            string value = "my-app-value";
+            System.IO.FileInfo file = new System.IO.FileInfo(testDir + @"\global-app.config");
+
+            //get writer
+            IConfigurations configs = new CommonAppWriter();
+
+            //write our value
+            configs.WriteValue(file, field, value);
+
+            //retrieve our value
+            string result = configs.GetValue(file, field);
+
+            Assert.AreEqual(value, result);
+        }
+
+    }
+}
